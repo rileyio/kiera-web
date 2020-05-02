07b1e55b875a7706865d30d8bc96d6a7f6a16c3a
Emma (RileyIO) <riley@tdm.io>
2020-05-02 11:18:01 -0300

    Removed CA Cert from vue dev config

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,6 @@ module.exports = {
   devServer: {
     host: 'dev.tdm.io',
     https: {
-      ca: fs.readFileSync('./ca.crt'),
       key: fs.readFileSync('./server.key'),
       cert: fs.readFileSync('./server.crt')
     }
